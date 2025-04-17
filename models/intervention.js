e2844001a58b692bb4eea39019bdea31e777e801
Veizh <maximedurville95300@gmail.com>
2025-04-17 17:24:08 +0200

    test

--- models/intervention.js
+++ models/intervention.js
@@ -5,6 +5,7 @@ const mongooseUniqueValidator = require('mongoose-unique-validator')
 var interventionSchemas = mongoose.Schema({
     clientName: { type: String,required:true},
     location:{type:String,required:true},
+    State:{type:String,required:false,default:"En cours"},
     materials:{type:Array,default:[]},
     contractNumber:{type:String,default:"XXXXXX"},
     startingDate:{type:String,require:true},
